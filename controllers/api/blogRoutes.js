fa4693b07318adef71091e87c9229109b3c5ec51
casukhelai <casukhelai@gmail.com>
2021-10-02 19:41:25 -0400

    creating blogRoutes

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,3 +1,22 @@
 const router = require('express').Router();
+const { title } = require('process');
 const { Post, User, Comment } = require('../../models');
 const authBlock = require('../../utils/auth');
+
+// get raw JSON of post for edit
+router.get('/:id', async (req, res) => {
+    try {
+        const data = await Post.findOne({ where: { id: req.params.id}});
+        const post = data.get({ plain: true});
+        res.status(200).json(post);
+    } catch(err){
+        res.status(404).json(err);
+    }
+});
+
+// create a new post
+
+
+// creates a new comment
+
+// update existing blog post
