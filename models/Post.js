aa69e352957cb4e05ac4c0dfc9e37091fbffd20c
casukhelai <casukhelai@gmail.com>
2021-10-02 19:22:26 -0400

    added model to post

--- models/Post.js
+++ models/Post.js
@@ -5,16 +5,28 @@ class Post extends Model {}
 
 Post.init(
     {
-
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        userName_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'User',
+                key: 'id',
+            },
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
     },
     {
-
-    },
-    {
-
-    },
-    {
-
+        sequelize,
+        timestamps: true,
     }
 );
 
