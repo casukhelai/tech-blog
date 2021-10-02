211e2bfb94dad9853a83c60d1223d0a8f550029f
casukhelai <casukhelai@gmail.com>
2021-10-02 19:23:12 -0400

    added model to user

--- models/User.js
+++ models/User.js
@@ -10,17 +10,52 @@ class User extends Model {
 
 User.init(
     {
-        
-    },
-    {
-
-    },
-    {
-
-    },
-    {
-
-    }
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              isAlphanumeric: true,
+            },
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+              isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+        },
+        {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+            },
+            beforeUpdate: async (updatedUserData) => {
+              updatedUserData.password = await bcrypt.hash(
+                updatedUserData.password,
+                10
+              );
+                return updatedUserData;
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'User',
+        }
 );
 
 module.exports = User;
