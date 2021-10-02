ed2d40bc07618c8b043133654f6664febe6a56f7
casukhelai <casukhelai@gmail.com>
2021-10-02 19:22:05 -0400

    added the models to comment

--- models/Comment.js
+++ models/Comment.js
@@ -5,16 +5,36 @@ class Comment extends Model {}
 
 Comment.init(
     {
-
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        userName_id: {
+            type: DataTypes.Integer,
+            allowNull: false,
+            references: {
+                model: 'User',
+                key: 'id',
+            }
+        },
+        bodyText: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'Post',
+                key: 'id',
+            },
+        },
     },
     {
-
-    },
-    {
-
-    },
-    {
-
+        sequelize,
+        timestamps: true,
     }
 );
 
