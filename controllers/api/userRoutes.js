d3a9976c2775bbcf2daf352cede0b7bd8c9fdf35
casukhelai <casukhelai@gmail.com>
2021-10-02 20:53:17 -0400

    added login validation

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -21,3 +21,32 @@ router.post('/signup', async (req,res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+    try {
+      // Need to check the emails, reject if invalid
+      const userData = await User.findOne({ where: { email: req.body.email } });
+      if (!userData) {
+        res.status(404).json({ message: 'Email does not match our records' });
+        return;
+      }
+  
+      // Check password too, reject if invalid
+      const isValid = await userData.checkPassword(req.body.password);
+      if (!isValid) {
+        res.status(404).json({ message: 'Password does not match our records' });
+        return;
+      }
+  
+      // Store the session and log the user in
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.loggedIn = true;
+        req.session.name = userData.name;
+  
+        res.status(200).json({ user: userData.name, message: 'You are now logged in!' });
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
